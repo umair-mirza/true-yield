5b20c2fbf5620be964f558d8048018af30d435ac
Umair Mirza <umair_hp@hotmail.com>
2022-09-12 20:29:56 +0500

    Added the contract and added some state variables

---
+++ contracts/EthStaker.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract EthStaker {
+
+    address public owner;
+
+    //Position is just amount of Ether staked by an address at a specific time for some duration
+    struct Position {
+        uint positionId;
+        address walletAddress; //That created the position
+        uint createdDate;
+        uint unlockDate; //When funds can be withdrawn without penalty
+        uint percentInterest;
+        uint weiStaked;
+        uint weiInterest; //Interest that the user will earn
+        bool open;
+    }
+
+    Position private position;
+
+    //It will increment after each new position is created
+    uint256 public currentPositionId;
+
+    //Every newly created position will be added to this mapping
+    mapping (uint => Position) public positions;
+
+    //For user to query all the positions that he has created
+    mapping (address => uint[]) public positionIdsByAddress;
+    
+    //Data for number of days and interest rates
+    mapping (uint => uint) public tiers;
+
+    //Array that contains integers for lock periods (30 days, 90 days, 365 days)
+    uint256 public lockPeriods;
+
+    //Payable to allow the deployer of the contract to send Ether to it when its being deployed
+    constructor() payable {
+        owner = msg.sender;
+    }
+}
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -4,18 +4,9 @@ const networkConfig = {
     },
     31337: {
         name: "localhost",
-        subscriptionId: "588",
-        keyHash: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
-        entranceFee: "1000000000000000", // 0.001 ETH
-        callbackGasLimit: "1000000", // 10,00000 gas
     },
     5: {
         name: "goerli",
-        subscriptionId: "16",
-        keyHash: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
-        entranceFee: "1000000000000000", // 0.001 ETH
-        callbackGasLimit: "1000000", // 10,00000 gas
-        vrfCoordinator: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
     },
     1: {
         name: "mainnet"
