b3dcb63a55e9eaa379fcb4847bde03f9444267f5
Umair Mirza <umair_hp@hotmail.com>
2022-09-19 00:06:01 +0500

    Added connect and data fetching functions to the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import react, {useState, useEffect} from "react"
-import ethers from 'ethers'
+import {ethers} from 'ethers'
 import artifacts from './artifacts/contracts/TrueYield.sol/TrueYield.json'
+import {bytesToStrings, toEther, toWei} from './helpers/helpers'
 import './App.css';
 
 const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
@@ -24,10 +25,97 @@ function App() {
   const [stakingLength, setStakingLength] = useState(undefined)
   const [stakingPercent, setStakingPercent] = useState(undefined)
   //Amount of Ether a user wants to stake
-  const [amount, ssetAmount] = useState(0)
+  const [amount, setAmount] = useState(0)
+
+  useEffect(() => {
+    const onLoad = async () => {
+      const provider = await new ethers.providers.Web3Provider(window.ethereum)
+      setProvider(provider)
+
+      const contract = await new ethers.Contract(
+        CONTRACT_ADDRESS,
+        artifacts.abi
+      )
+      setContract(contract)
+    }
+    onLoad()
+  }, [])
+
+  const isConnected = () => signer !== undefined
+
+  const getSigner = async () => {
+    //From Ethers.js to get a signer to call functions
+    provider.send("eth_requestAccounts", [])
+    const signer = provider.getSigner()
+    return signer
+  }
+
+  const getAssetIds = async (address, signer) => {
+    const assetIds = await contract.connect(signer).getAllPositionIdsByAddress(address)
+    return assetIds
+  }
+
+  const getAssets = async (ids, signer) => {
+    //Using a Promise.all so that we can wait until we can the data for all the positii=ons
+    const queriedAssets = await Promise.all(
+      ids.map(id => contract.connect(signer).getPositionById(id))
+    )
+
+    queriedAssets.map(async asset => {
+      //Will be easy to work with an object of the data that comes back with getPositionById
+      const parsedAsset = {
+        positionId: asset.positionId,
+        percentInterest: Number(asset.percentInterest) / 100,
+        daysRemaining: calculateRemainingDays(Number(asset.unlockDate)),
+        etherInterest: toEther(asset.weiInterest),
+        etherStaked: toEther(asset.weiStaked),
+        open: asset.open,
+      }
+
+      setAssets((prev) => [...prev, parsedAsset])
+    })
+  }
+
+  const calculateRemainingDays = (unlockDate) => {
+    const timeNow = Date.now() / 1000 //as Date.now() returns in milliseconds
+    const remainingSeconds = unlockDate - timeNow
+    return Math.max((remainingSeconds / 60 / 60 / 24).toFixed(0), 0) //To return result in days without decimals and Max of that number and hundred so that there is no negative number and past date is shown as 0
+  }
+
+  const connectAndLoad = async () => {
+    const signer = await getSigner(provider)
+    setSigner(signer)
+
+    const signerAddress = await signer.getAddress()
+    setSignerAddress(signerAddress)
+
+    const assetIds = await getAssetIds(signerAddress, signer)
+    setAssetIds(assetIds)
+
+    getAssets(assetIds, signer)
+  }
+
+  const openStakingModal = (stakingLength, stakingPercent) => {
+    setShowStakeModal(true)
+    setStakingLength(stakingLength)
+    setStakingPercent(stakingPercent)
+  }
+
+  const stakeEther = () => {
+    const wei = toWei(amount)
+    const data = {value: wei}
+    contract.connect(signer).stakeEther(stakingLength, data)
+  }
+
+  const withdraw = positionId => {
+    contract.connect(signer).closePosition(positionId)
+  }
 
   return (
     <div>
+      <div>
+        <Header isConnected={isConnected} connect={connectAndLoad} />
+      </div>
       <div className="mt-20 text-center">
         <div className="font-extrabold text-transparent text-5xl md:text-7xl bg-clip-text bg-gradient-to-b from-gray-300 via-gray-300 to-purple-300 tracking-wide">
           TrueYield
