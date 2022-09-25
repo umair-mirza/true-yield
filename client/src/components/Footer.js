1fd53f32fff2a82df02c70cd8d60dbb9e4ce92a3
Umair Mirza <umair_hp@hotmail.com>
2022-09-25 18:46:14 +0500

    Did some minor responsive adjustments to the frontend

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="mt-8 mb-3 text-center">
-        <div className="px-2 py-1 mx-auto bg-gray-800 inline-block text-gray-200 rounded shadow-lg">
+        <div className="px-2 py-1 text-xs lg:text-sm mx-auto bg-gray-800 inline-block text-gray-200 rounded shadow-lg">
             <span className="font-bold">Disclaimer:</span> <span>This is an experimental project deployed on Goerli Testnet</span>
         </div>
     </div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,9 +5,9 @@ const Header = ({isConnected, connect}) => {
     <>
     {
         isConnected() ? (
-            <button className="px-4 py-2 bg-gradient-to-b from-gray-800 via-gray-700 to-purple-800 text-gray-100 rounded-full shadow-md shadow-fuchsia-300">Connected</button>
+            <button className="px-4 py-2 bg-gradient-to-b from-gray-800 via-gray-700 to-purple-800 text-xs sm:text-sm lg:text-base text-gray-100 rounded-full shadow-md shadow-fuchsia-300">Connected</button>
         ) : (
-            <button onClick={() => connect()} className="px-4 py-2 bg-gradient-to-b from-gray-800 via-gray-700 to-purple-800 text-gray-100 rounded-full shadow-md shadow-fuchsia-300 hover:shadow-fuchsia-600 hover:font-semibold transition-shadow duration-200">Connect Wallet</button>
+            <button onClick={() => connect()} className="px-4 py-2 bg-gradient-to-b from-gray-800 via-gray-700 to-purple-800 text-xs sm:text-sm lg:text-base text-gray-100 rounded-full shadow-md shadow-fuchsia-300 hover:shadow-fuchsia-600 hover:font-semibold transition-shadow duration-200">Connect Wallet</button>
         )
     }
     </>
